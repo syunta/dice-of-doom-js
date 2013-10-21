bd571075af4924c515b8293d5d7661e4a8fc00fb
syunta kitagawa <kitagawa@timedia.co.jp>
2013-10-21 15:06:59 +0900

    gameTableをオブジェクトで定義したため参照渡しが発生し、setInitialGameTable()に引数として渡していた意味がなかったので、gameTableはsetInitialGameTable内で定義することにする

--- app.js
+++ app.js
@@ -3,35 +3,12 @@ $(function(){
     var TABLE_SIZE = 2;
     var TABLE_ROW = ['A','B','C','D'];
     var TABLE_COLUMN = [1,2,3,4];
-    
-    var gameTable = {
-        'A1':{
-            owner:null,
-            dice:0,
-            link:['A2','B1','B2']
-        },
-        'A2':{
-            owner:null,
-            dice:0,
-            link:['A1','B2']
-        },
-        'B1':{
-            owner:null,
-            dice:0,
-            link:['A1','B2']
-        },
-        'B2':{
-            owner:null,
-            dice:0,
-            link:['A1','A2','B1']
-        }
-    }; 
 
     startApp();
 
     function startApp(){
         drawGameTable();
-        setInitialGameTable(gameTable);
+        var gameTable = setInitialGameTable();
         console.log(gameTable['A1']);
         console.log(gameTable['A2']);
         console.log(gameTable['B1']);
@@ -56,6 +33,28 @@ $(function(){
     }
 
     function setInitialGameTable(gameTable){
+        var gameTable = {
+            'A1':{
+                owner:null,
+                dice:0,
+                link:['A2','B1','B2']
+            },
+            'A2':{
+                owner:null,
+                dice:0,
+                link:['A1','B2']
+            },
+            'B1':{
+                owner:null,
+                dice:0,
+                link:['A1','B2']
+            },
+            'B2':{
+                owner:null,
+                dice:0,
+                link:['A1','A2','B1']
+            }
+        }; 
         var players = ['A','B'];
         for(var i = 0; i < TABLE_SIZE; i++){
             for(var j = 0; j < TABLE_SIZE; j++){
@@ -63,6 +62,8 @@ $(function(){
                 gameTable[ TABLE_ROW[i]+TABLE_COLUMN[j] ].dice = getRandom(1,3);
             }
         }
+
+        return gameTable;
     }
 
     function getRandom(min,max){
