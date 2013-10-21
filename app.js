4baca83c482935496bebb94a76a97f1d6bae79d5
syunta kitagawa <kitagawa@timedia.co.jp>
2013-10-21 14:24:38 +0900

    描画用変数gameTableをtableFrameに名前変更、さらに盤面の詳細な状態を表すgameTableを再定義、分かりやすくするためべったりと記述

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 $(function(){
     
     var TABLE_SIZE = 2;
+    
+    var gameTable = {
+        'A1':{
+            owner:null,
+            dice:0,
+            link:['A2','B1','B2']
+        },
+        'A2':{
+            owner:null,
+            dice:0,
+            link:['A1','B2']
+        },
+        'B1':{
+            owner:null,
+            dice:0,
+            link:['A1','B2']
+        },
+        'B2':{
+            owner:null,
+            dice:0,
+            link:['A1','A2','B1']
+        }
+    }; 
 
     startApp();
 
     function startApp(){
         drawGameTable();
+        console.log(gameTable['A1']);
+        console.log(gameTable['B1']);
     }
 
     function drawGameTable(){
-        var gameTable = '';
+        var tableFrame = '';
         var tableColumns = ['A','B','C','D'];
         var space = '&nbsp;&nbsp;';
 
         for(var i = 0; i < TABLE_SIZE; i++){
             for(var j = TABLE_SIZE; i < j; j--){
-                gameTable += space;
+                tableFrame += space;
             }
             for(var j = 0; j < TABLE_SIZE; j++ ){
-                gameTable += tableColumns[i] + (i+1) + space;
+                tableFrame += tableColumns[i] + (j+1) + space;
             }
 
-            gameTable += '</br>';
+            tableFrame += '</br>';
         }
-        $("body").html(gameTable);
+        $("body").html(tableFrame);
     }
 });
